22b6b7520d9f64be1a5136d2b65ba675b2217cc2
wJumelle <wjumelle@gmail.com>
2021-08-02 19:39:02 +0200

    Ai-je réparé mon problème de branche ? Il se peut que oui.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+/*
+ = Blocage avec ecouteur d'event handleCardClick sur .card__img--front plutot que .card
+ = Si on laisse l'application de l'effet sur .card on perd le positionnement des autres cartes car .card devient position fixed
+ */
+
 window.onload = () => {
     // Variables
     const cards = document.querySelectorAll('.card');
@@ -8,10 +13,76 @@ window.onload = () => {
         element.style.height = cardsHeight + 'px';
         element.style.width = cardsWidth + 'px';
 
-        element.addEventListener('click', function(e) {
-            this.classList.add('card--clicked');
+        element.querySelector('.card__perspective').style.height = cardsHeight + 'px';
+        element.querySelector('.card__perspective').style.width = cardsWidth + 'px';
 
-            //setTimeout(() => this.classList.remove('card--clicked'), 2000);
-        }, false);
+        const cardFront = element.querySelector('.card__img--front');
+
+        element.addEventListener('click', handleCardClick);
     });
+}
+
+// Fonctions
+// Animation de la carte au click
+function handleCardClick() {
+    const cardPos = localToGlobal(this);
+    
+    this.style.position = 'fixed';
+    this.style.top = cardPos.top + 'px';
+    this.style.left = cardPos.left + 'px';
+    this.dataset.position = `{ "top": ${cardPos.top}, "left": ${cardPos.left} }`;
+    
+    this.classList.add('card--clicked');
+
+    setTimeout(() => {
+        this.style.top = '50%';
+        this.style.left = '50%';
+        this.style.transform = 'translate(-50%, -50%) rotateY(360deg)';
+
+        this.removeEventListener('click', handleCardClick);
+        this.addEventListener('click', handleCardReturnToInitialPos);
+    }, 100);
+}
+
+function handleCardReturnToInitialPos() {
+    const cardInitialPos = JSON.parse(this.dataset.position);
+    this.style.top = cardInitialPos.top + 'px';
+    this.style.left = cardInitialPos.left + 'px';
+    this.style.transform = 'translate(0, 0) rotateY(0)';
+
+    setTimeout(() => {
+        this.classList.remove('card--clicked');
+        this.removeEventListener('click', handleCardReturnToInitialPos);
+        this.addEventListener('click', handleCardClick);
+    }, 2000);
+}
+
+// Récupération de la position d'un élément par rapport au viewport
+function localToGlobal( _el ) {
+    var target = _el,
+    target_width = target.offsetWidth,
+    target_height = target.offsetHeight,
+    target_left = target.offsetLeft,
+    target_top = target.offsetTop,
+    gleft = 0,
+    gtop = 0,
+    rect = {};
+
+    var moonwalk = function( _parent ) {
+        if (!!_parent) {
+            gleft += _parent.offsetLeft;
+            gtop += _parent.offsetTop;
+            moonwalk( _parent.offsetParent );
+        } else {
+            return rect = {
+            top: target.offsetTop + gtop,
+            left: target.offsetLeft + gleft,
+            bottom: (target.offsetTop + gtop) + target_height,
+            right: (target.offsetLeft + gleft) + target_width
+            };
+        }
+    }
+
+    moonwalk( target.offsetParent );
+    return rect;
 }
